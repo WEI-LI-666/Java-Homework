376b9345cea3ad30e700ec57685ebdc9ed04c5d2
PowerChen <zxc851023@gmail.com>
2018-12-01 21:17:06 +0800

    homework update

---
+++ hw9/src/hw9/TimeFormatException.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 11.2018.  by Willy
+ */
+
+package hw9;
+
+public class TimeFormatException extends Exception{
+
+    public TimeFormatException(String message) {
+        super(message);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
